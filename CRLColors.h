56dddf27fbcd6e3b0d190140c29c177ce077a791
HDM Ltd <hdm@HDMs-Mac-mini.local>
2013-03-25 16:47:54 +0400

    Created color model

---
+++ CRLColors.h
@@ -0,0 +1,13 @@
+//
+//  CRLColors.h
+//  ColorList
+//
+//  Created by HDM Ltd on 3/25/13.
+//  Copyright (c) 2013 HDM Ltd. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface CRLColors : NSObject
+
+@end
---
+++ CRLColors.m
@@ -0,0 +1,13 @@
+//
+//  CRLColors.m
+//  ColorList
+//
+//  Created by HDM Ltd on 3/25/13.
+//  Copyright (c) 2013 HDM Ltd. All rights reserved.
+//
+
+#import "CRLColors.h"
+
+@implementation CRLColors
+
+@end
